e713702dd726aa04766acdbf8066b2dc86350743
David Alejandro Quezada Verduga <davidalejandro.quezada@estudiants.urv.cat>
2024-12-09 22:31:04 +0100

    Professor, xerrades i una mica de main

---
+++ scr/dades/Membre.java
@@ -0,0 +1,52 @@
+package dades;
+
+public class Membre {
+    private String alias;
+    private String emailInstitucional;
+    private String dataAlta;
+    private String dataBaixa;
+
+    public Membre(String alias, String emailInstitucional, String dataAlta) {
+        this.alias = alias;
+        this.emailInstitucional = emailInstitucional;
+        this.dataAlta = dataAlta;
+        this.dataBaixa = null;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getEmailInstitucional() {
+        return emailInstitucional;
+    }
+
+    public String getTipus() {
+        return "membre"; 
+    }
+
+    public String getDataAlta() {
+        return dataAlta;
+    }
+
+    public String getDataBaixa() {
+        return dataBaixa;
+    }
+
+    public void setDataBaixa(String dataBaixa) {
+        this.dataBaixa = dataBaixa;
+    }
+
+    public boolean isActiu() {
+        return dataBaixa == null;
+    }
+
+    public Membre copia() {
+        return new Membre(alias, emailInstitucional, dataAlta);
+    }
+
+    @Override
+    public String toString() {
+        return alias + ";" + emailInstitucional + ";" + dataAlta + ";" + (dataBaixa != null ? dataBaixa : "null");
+    }
+}
