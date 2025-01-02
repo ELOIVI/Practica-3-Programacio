2c8a01016f980b354d9694dcc0ddacbd06dc9ce3
TheFermi7 <ferminlloret@gmail.com>
2025-01-02 18:34:30 +0100

    Fermin

--- ExcepcioMembreJaExisteix.java
+++
@@ -1,7 +0,0 @@
-package excepcions;
-
-public class ExcepcioMembreJaExisteix extends Exception {
-    public ExcepcioMembreJaExisteix(String missatge) {
-        super(missatge);
-    }
-}
--- ExcepcioMembreNoTrobat.java
+++
@@ -1,7 +0,0 @@
-package excepcions;
-
-public class ExcepcioMembreNoTrobat extends Exception {
-    public ExcepcioMembreNoTrobat(String missatge) {
-        super(missatge);
-    }
-}
