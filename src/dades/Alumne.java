88c510339f0eb36f6e28e8d45b7a9fc6ea2d6316
TheFermi7 <ferminlloret@gmail.com>
2025-01-02 21:13:30 +0100

    Fermin.2

---
+++ src/dades/Alumne.java
@@ -0,0 +1,34 @@
+package dades;
+
+public class Alumne extends Membre {
+    private String ensenyament;
+    private int anysETSE;
+
+    public Alumne(String alias, String emailInstitucional, String dataAlta, String ensenyament, int anysETSE) {
+        super(alias, emailInstitucional, dataAlta); // Crida explícita al constructor de la classe Membre
+        this.ensenyament = ensenyament;
+        this.anysETSE = anysETSE;
+    }
+
+    public String getEnsenyament() {
+        return ensenyament;
+    }
+
+    public int getAnysETSE() {
+        return anysETSE;
+    }
+
+    public String getTipus() {
+        return "alumne"; // Retorna el tipus com a "alumne"
+    }
+
+    public void setGraduat() {
+        this.anysETSE = -1; // Indica que l'alumne està graduat.
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ";" + ensenyament + ";" + anysETSE;
+    }
+}
+
